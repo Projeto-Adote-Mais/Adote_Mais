73fe031bd1206a304dbdacaecc02170e4554efb0
Larissa Carvalho <carvalholarissa530@gmail.com>
2025-11-21 12:49:16 -0300

    Ajustes na página dúvidas e remoção de imagens/ícones não utilizados

--- js/duvidas.js
+++ js/duvidas.js
@@ -12,3 +12,18 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+const btnMobile = document.getElementById("btn-mobile");
+const navMobile = document.getElementById("nav-mobile");
+
+// Ativa nav mobile
+btnMobile.addEventListener("click", () => {
+  navMobile.classList.toggle("active");
+});
+
+// Esconde nav mobile com relação ao tamanho da janela
+window.addEventListener("resize", () => {
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    navMobile.classList.remove("active");
+  }
+});
